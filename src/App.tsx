93a81e082bb8a0a1376b209b695dc3a06edcc92d
gapur <kaloycant@gmail.com>
2023-02-22 18:25:39 +0100

    styles and themes, context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { Suspense, useContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
+import classNames from "classnames";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { Suspense } from "react";
+
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
+
+
 
 const App: React.FC = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={classNames("app", theme)}>
       <div style={{ display: "flex", gap: "0.2em" }}>
+        <button onClick={toggleTheme}>
+          {theme}
+        </button>
         <Link to={"/"}>Main</Link>
-        {}
         <Link to={"/about"}>About</Link>
       </div>
       <Suspense fallback={<div>Loading...</div>}>
