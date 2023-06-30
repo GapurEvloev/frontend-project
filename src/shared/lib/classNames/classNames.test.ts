c287be12a1e8d0e84369da748a288167e35cb69b
GapurEvloev <kaloycant@gmail.com>
2023-06-30 03:48:52 +0200

    module 2: l20 - setting jest, first test for classNames

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,27 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional params', () => {
+    expect(classNames('someClass', {}, ['additionalClass1', 'additionalClass2'])).toBe('someClass additionalClass1 additionalClass2');
+  });
+
+  test('with mods', () => {
+    expect(classNames('someClass', { hovered: true, scrollable: true })).toBe('someClass hovered scrollable');
+  });
+
+  test('with false mod', () => {
+    expect(classNames('someClass', { hovered: true, scrollable: false })).toBe('someClass hovered');
+  });
+
+  test('with undefined mod', () => {
+    expect(classNames('someClass', { hovered: true, scrollable: undefined })).toBe('someClass hovered');
+  });
+
+  test('with additional and mod', () => {
+    expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['additionalClass1', 'additionalClass2'])).toBe('someClass additionalClass1 additionalClass2 hovered');
+  });
+});
