7bb6fa142a6700ef669091f82999295d117fa329
GapurEvloev <kaloycant@gmail.com>
2023-07-05 02:09:48 +0200

    module 2: l23 - BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -18,7 +19,9 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev), // isDev constant
     }),
-
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false, // Disable open analyzer
+    }), // Bundle analyzer plugin
   ];
 
   if (isDev) {
