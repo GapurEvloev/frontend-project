91bad3b4ee715da733a72cd01654ce9302dbb048
gapur <kaloycant@gmail.com>
2023-08-05 23:04:59 +0200

    module 4: l31.1 - fixes for ci 15

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,10 +1,16 @@
-import React, { FC, useEffect, useMemo } from 'react';
+import React, {
+  FC, useEffect, useMemo, useState,
+} from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
-  const [theme, setTheme] = React.useState<Theme>(defaultTheme);
+interface ThemeProviderProps {
+  initialTheme?: Theme;
+}
+
+const ThemeProvider: FC<ThemeProviderProps> = ({ children, initialTheme }) => {
+  const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
   const defaultProps = useMemo(() => ({
     theme,
--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,10 @@
 import { Story } from '@storybook/react';
-import { Theme, ThemeSBProvider } from 'app/providers/ThemeProvider';
+import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
-  <ThemeSBProvider initialTheme={theme}>
+  <ThemeProvider initialTheme={theme}>
     <div className={`app ${theme}`}>
       <StoryComponent />
     </div>
-  </ThemeSBProvider>
+  </ThemeProvider>
 );
