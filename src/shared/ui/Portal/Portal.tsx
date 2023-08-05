f71396387213c454b3cf696aa03712b32e826551
gapur <kaloycant@gmail.com>
2023-08-05 18:58:49 +0200

    module 4: l31.1 - fixes for ci 7

--- src/shared/ui/Portal/Portal.tsx
+++ src/shared/ui/Portal/Portal.tsx
@@ -9,4 +9,4 @@ interface PortalProps {
 export const Portal = ({
   children,
   element,
-}: PortalProps) => createPortal(children, element || document.body);
+}: PortalProps) => createPortal(children, element || document.getElementById('root'));
